134f0f489b85c5991a4df52f2484e76be32da8a8
OwO0727 <88077998+OwO0727@users.noreply.github.com>
2024-01-17 17:13:19 +0000

    Update main.py

--- client/main.py
+++ client/main.py
@@ -4,10 +4,33 @@ from game import *
 from player import *
 from tiles import *
 
+player1 = Player("Jianne")
+player2 = Player("Bot1")
+player3 = Player("Bot2")
+player4 = Player("Bot3")
+
 game = Game()
-tiles = [2,2,2,3,3,3,4,4,4,31,31,31,32]
-new_tiles = numToTiles(tiles)
-print(tiles)
-deck = Deck(new_tiles)
-win = deck.determine_last_tile(game)
-print(win)
+
+game.add_players(player1)
+game.add_players(player2)
+game.add_players(player3)
+game.add_players(player4)
+
+game.initialize_tiles()
+game.initial_deck()
+
+for i in game.get_players():
+    game.show_info(i)
+    print()
+
+count = 0
+player = game.get_players()[0] 
+player.get_deck().draw_tile()
+while game.get_condition() != True:
+    next = game.round(count)
+    count = next
+
+for player in game.get_players():
+    if player.get_win():
+        print(f"winner is {player.get_name()}")
+
