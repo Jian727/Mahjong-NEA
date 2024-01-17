589f60c71285bf21ff0d27772019a09f2b76abc9
OwO0727 <88077998+OwO0727@users.noreply.github.com>
2024-01-17 17:12:07 +0000

    Update game.py

--- client/game.py
+++ client/game.py
@@ -6,7 +6,7 @@ from deck import *
 class Game:
     def __init__(self):
         self.tiles = [] #represent all the tiles
-        self.tilesremain= self.initialize_tiles() #represent the avaliable tiles on table 
+        self.tilesremain= [] #represent the avaliable tiles on table 
         self.tilesoutside=[] #represent the tiles discarded on table
         self.roomnum=0
         self.playerconns=[]
@@ -22,6 +22,45 @@ class Game:
     async def getqueue(self):
         temp=await self.queue.get()
         return temp
+    
+    def get_tilesoutside(self):
+        return self.tilesoutside
+    
+    def pop_tilesoutside(self):
+        self.tilesoutside.pop(-1)
+    
+    def get_tilesremain(self):
+        return self.tilesremain
+    
+    def get_tiles(self):
+        return self.tiles
+    
+    def get_players(self):
+        return self.players
+
+    def get_condition(self):
+        for i in self.players:
+            if i.get_deck().determine_winning_deck() == True:
+                self.endgame()
+                i.set_win()
+        if len(self.get_tilesremain()) == 0:
+            self.endgame()
+        return self.condition
+    
+    def draw_tile(self):
+        if len(self.tilesremain) != 0:
+            tile = self.tilesremain[0]
+            self.tilesremain.remove(tile)
+            return tile
+        else:
+            self.endgame()
+            return None
+    
+    def add_tilesoutside(self, tile):
+        self.tilesoutside.append(tile)
+
+    def add_players(self, player):
+        self.players.append(player)
     
     def initialize_tiles(self):
         # Define the set of Mahjong tiles
@@ -51,38 +90,83 @@ class Game:
             
             onetime = False
 
-        #bonus
-        for value in range(8):
-            tiles.append(Bonus(value))
-
         random.shuffle(tiles)
-        return tiles
+        self.tilesremain = tiles
+
+    def show_info(self, player):
+        global deck, showed
+        print(f"Name: {player.get_name()}")
+        deck, showed = self.display_tiles(player)
+        print(f"deck: {tilesToNum(deck)}, showed: {tilesToNum(showed)}")
+
+    def round(self, player_num): #need to return next player_num
+        #discard
+        player = self.get_players()[player_num]
+        player_deck  = player.get_deck()
+        self.show_info(player)
+        discard = int(input("Please input the tile you want to discard: "))
+        index = tilesToNum(deck).index(discard)
+        player_deck.discard_tile(index)
+        print(f"tile discarded, tiles remain:{len(self.get_tilesremain())}")
+        print(f"tile outside: {tilesToNum(self.get_tilesoutside())}")
+        print()
+
+        #check pung
+        count_temp = player_num + 1
+        for i in range(3): #pung check 
+            count_temp = count_temp % 4
+            player_check = self.get_players()[count_temp]
+            print(f"player pung check: {player_check.get_name()}")
+            player_deck_check = player_check.get_deck()
+            pungset = player_deck_check.findPung()
+            if pungset != None:
+                self.show_info(player_check)
+                decision = input(f"Name: {player_check.get_name()} can pung {tilesToNum(pungset)}! do you want to pung? (y/n)")
+                if decision == "y":
+                    player_deck_check.Pung(pungset)
+                    print()
+                    return count_temp
+                else:
+                    print()
+            count_temp += 1
+
+        #check next player chow
+        next_player_num = (player_num + 1)%4
+        next_player = self.get_players()[next_player_num]
+        next_player_deck = next_player.get_deck()
+        print(f"player chow check: {next_player.get_name()}")
+        chowsets = next_player_deck.findChow()
+        if chowsets != None: #chow check
+            print("have chow")
+            for chowset in chowsets:
+                self.show_info(next_player)
+                decision = input(f"Name: {next_player.get_name()} can chow {tilesToNum(chowset)}! do you want to chow? (y/n)")
+                if decision == "y":
+                    next_player_deck.Chow(chowset)
+                    print()
+                    return next_player_num
+        print()
+        self.get_players()[next_player_num].get_deck().draw_tile() 
+        return next_player_num
     
     def initial_deck(self):
         #initiallise deck for each player
         for player in self.players:
             hand = []
             for i in range(13):
-                tile = self.tilesremain.pop(0)
+                tile=self.tilesremain.pop(0)
                 hand.append(tile)
-            player_deck = Deck(hand)
+            player_deck = Deck(hand, self)
             player.set_deck(player_deck)
-
+            
     def display_tiles(self, player):
         #display deck for the player stated
         for i in self.players:
             if i == player:
-                deck = i.get_deck()
-                return deck
-    
-    def add_tilesoutside(self, tile):
-        self.tilesoutside.append(tile)
-    
-    def get_tilesoutside(self):
-        return self.tilesoutside
-    
-    def get_tilesremain(self):
-        return self.tilesremain
-    
-    def get_tiles(self):
-        return self.tiles
+                deck = i.get_deck_tiles()
+                displayed = i.get_showed()
+                return [deck,displayed]
+            
+    def endgame(self):
+        #end the game by having winner or used all tiles
+        self.condition = True
