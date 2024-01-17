d3b8dbc190d58ce990903bab673eddf643bb36e5
OwO0727 <88077998+OwO0727@users.noreply.github.com>
2024-01-17 17:11:42 +0000

    Update functions.py

--- client/functions.py
+++ client/functions.py
@@ -6,15 +6,20 @@ def checksubset(array1, array2): #if array2 a subset array1
     counter_array2 = Counter(array2)
     return all(count <= counter_array1[key] for key, count in counter_array2.items())
 
-def remove_tiles(deck, tiles):
-    return [x for x in tiles if x not in deck]
 
 def checkPung(tiles):
     return all(elem.get_cal_value() == tiles[0].get_cal_value() for elem in tiles)
 
-def checkChow(tiles):
-    values = [obj.get_cal_value() for obj in tiles]
-    return (values == list(range(values[0], values[-1] + 1)))
+def checkChow(tiles): #check attribute in same suit
+    for i in tiles:
+        if isinstance(i, Honor):
+            return False
+    tile1, tile2, tile3 = tiles
+    if tile1.get_type() == tile2.get_type() == tile3.get_type():
+        values = [obj.get_cal_value() for obj in tiles]
+        values.sort()
+        return (values == list(range(values[0], values[-1] + 1)))
+    return False
 
 def checkSameTileInArray(tile, array):
     for i in array:
@@ -25,7 +30,17 @@ def checkSameTileInArray(tile, array):
 #functions for testing only
 
 def tilesToNum(tiles):
-    return [obj.get_cal_value() for obj in tiles]
+    result =[]
+    
+    for tile in tiles:
+        if type(tile) is list:
+            temp_result=[]
+            for i in tile:
+                temp_result.append(i.get_cal_value())
+            result.append(temp_result)
+        else:
+            result.append(tile.get_cal_value())
+    return result
 
 def numToTiles(num):
     result = []
@@ -39,3 +54,5 @@ def numToTiles(num):
         value = i-type
         result.append(Tiles(type, value))
     return result
+
+
